864a73cdc9c388c21b4eb6f5926d485e768c70ba
Edvard Stoneson <43728607+nomad-usa@users.noreply.github.com>
2021-05-13 22:53:50 +0600

    test

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const express = require('express');
-const 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('request made');
+});
+
+// localhost is the default value for 2nd argument
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
